f2eba578cc5ab95ec6eb58ed3bea3a225f42545c
Ati <atiar.khan@edg.com.au>
2026-07-08 16:53:50 +1000

    Rename sheet from 'PIM_Game' to 'PIM_Odyssey_DB'

--- pim-play.py
+++ pim-play.py
@@ -431,7 +431,7 @@ QUIZ_POOL = [
 # ═══════════════════════════════════════════════════════════════════
 # DATA LAYER
 # ═══════════════════════════════════════════════════════════════════
-SHEET_NAME     = "PIM_Game"
+SHEET_NAME     = "PIM_Odyssey_DB"
 WORKSHEET_NAME = "Quiz"
 SCOPES         = [
     "https://spreadsheets.google.com/feeds",
